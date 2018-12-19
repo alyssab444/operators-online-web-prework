d5ff040467350b9da5d408fc796723f85cf47cc9
Alyssa Brown <alyssa.brown@live.com>
2018-12-19 02:58:37 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,12 +2,12 @@ def unsafe?(speed)
   speed < 40 
  else speed > 60 
 end
-https://learn.co/tracks/web-development-fundamentals/intro-to-ruby/conditionals/operators-and-conditionals?batch_id=768&track_id=10415#  
+
 
 
 
 def not_safe?(speed)
-  speed > 60 
+  speed > 60 ? : < 40
 end
 	
 
